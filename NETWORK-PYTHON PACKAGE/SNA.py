558163f205fb254de10b7a2b8bae8187552eedc7
Jake Lund <lundj3@myumanitoba.ca>
2019-11-30 12:00:26 -0600

    updted python code, added sentemental java code

--- NETWORK-PYTHON PACKAGE/SNA.py
+++ NETWORK-PYTHON PACKAGE/SNA.py
@@ -7,48 +7,56 @@ G=nx.MultiGraph()
 
 # adding just one node:
 G.add_node("london", seats=.425) # single node with a value seats (seats being a form of weight)
-G.add_node("Birmingham", seats=.057)
+G.add_node("birmingham", seats=.057)
 G.add_node("liverpool", seats=.051)
-G.add_node("Glasgow", seats=.023)
-G.add_node("Edinburgh", seats=.018)
-G.add_node("Cardiff", seats=.018)
-G.add_node("Newport", seats=.014)
-G.add_node("Swansea", seats=.014)
-G.add_node("Belfast", seats=.012)
-G.add_node("Aberdeen", seats=.010)
-G.add_node("Derry", seats=.004)
-G.add_node("Lisburn", seats=.004)
+G.add_node("glasgow", seats=.023)
+G.add_node("edinburgh", seats=.018)
+G.add_node("cardiff", seats=.018)
+G.add_node("newport", seats=.014)
+G.add_node("swansea", seats=.014)
+G.add_node("belfast", seats=.012)
+G.add_node("aberdeen", seats=.010)
+G.add_node("derry", seats=.004)
+G.add_node("lisburn", seats=.004)
 
 G.add_node("Labour-Party")
 G.add_node("Conservative-Party")
 G.add_node("Liberal-Democrats")
 G.add_node("Brexit-Party")
 
-filename = os.path.abspath(os.path.join("data", "londonSentiment.csv"))
-print(filename)
+dircName = "Calculated data"
 
-with open(filename, 'r') as fin:
-    header = fin.readline().strip().split(',')
-    print(header)
 
-    entries = []
-    for line in fin:
-        parts = line.strip().split(',')
-        row = dict()
-        for i, h in enumerate(header):
-            row[h] = parts[i]
+for subdir, dirs, files in os.walk(dircName):
+    for file in files:
+        print(subdir)
+        print(dirs)
+        print(files)
+        filename = os.path.abspath(os.path.join(subdir, file))
+        with open(filename, 'r') as fin:
+            header = fin.readline().strip().split(',')
+            print(header)
+            entries = []
+            for line in fin:
+                parts = line.strip().split(',')
+                row = dict()
+                for i, h in enumerate(header):
+                    row[h] = parts[i]
 
-        entries.append(row)
-
-# entries.sort(key=lambda r: int(r['Sentiment']))   ***sort
-
-# while running through data assign the edges based of City, Party, sentimental value
-for (e) in entries:
-    print("{0}, {1}, sentiment: {2}".format(e['City'], e['Party'], e['Sentiment']))
-    city = e['City']
-    party = e['Party']
-    weight = float(e['Sentiment'])*(G.nodes[city]['seats'])
-    G.add_edge(city, party, weight=weight)
+                entries.append(row)
+        # while running through data assign the edges based of City, Party, sentimental value
+        for (e) in entries:
+            # print("{0}, {1}, {2} sentiment: {3}".format(e['City'], e['Party'], e['Leader'], e['Sentiment']))
+            city = e['City']
+            party = e['Party']
+            leader = e['Leader']
+            #if city == "london" or "birmingham" or "glasgow" or "edinburgh" or "cardiff" or"newport" or "swansea" or "belfast" or "aberdeen" or "derry" or "lisburn":
+            #weight = float(e['Sentiment Value'])
+            print(city)
+            weight = float(e['Sentiment Value'])*(G.nodes[city]['seats'])
+            G.add_edge(city, party, weight=weight)
+            if(party == ""):
+                G.add_edge(city, leader, weight=weight)
 
 # printing nodes
 print("Print all Nodes")
@@ -67,35 +75,39 @@ totalConsPos = 0
 totalConsNeg = 0
 
 for (c, p, w) in G.edges.data('weight'):
-#    print('(%s, %s, %f' % (c, p, w)) # printing edges with their weight
     tempWeight = w
     if tempWeight < 0:
-        tempWeight *= .3
-    if p == "Labour-Party":
+        tempWeight = 0
+    if p == "Labour-Party" or p == "Jeremy-Corbyn":
+        labourWeight += tempWeight
         if w > 0:
             totalLabourPos += 1
         elif w < 0:
             totalLabourNeg += 1
-        labourWeight += tempWeight
-    elif p == "Conservative-Party":
+    elif p == "Conservative-Party" or p == "Borris-Johnson":
+        conservativeWeight += tempWeight
         if w > 0:
             totalConsPos += 1
         elif w < 0:
             totalConsNeg += 1
-        conservativeWeight += tempWeight
-    elif p == "Liberal-Democrats":
+    elif p == "Liberal-Democrats" or p == "jo-Swinson":
         liberalDemocratWeight += tempWeight
-    elif p == "Brexit-Party":
+        if w > 0:
+            totalLabourPos += 1
+        elif w < 0:
+            totalLabourNeg += 1
+    elif p == "Brexit-Party" or p == "Nigel-Farage":
         brexitWeight += tempWeight
+        if w > 0:
+            totalLabourPos += 1
+        elif w < 0:
+            totalLabourNeg += 1
 
-print(labourWeight)
+
 print(conservativeWeight)
+print(labourWeight)
 print(liberalDemocratWeight)
 print(brexitWeight)
-print(totalLabourPos)
-print(totalLabourNeg)
-print(totalConsPos)
-print(totalConsNeg)
 
 
 
@@ -103,6 +115,6 @@ print(totalConsNeg)
 # print(type(G.edges()))
 
 # print the network as a graph
-nx.draw(G)
+nx.draw(G, with_labels=True, node_color='skyblue', node_size=250, edge_color='black', pos=nx.planar_layout(G))
 plt.savefig("simple_path.png")
 plt.show()
---
+++ Sentimental.java
@@ -0,0 +1,155 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*; 
+/*
+ * Version:2.0
+ * Note: RAW_DATA folder should have the B1-B12 folders containing raw data and SENTIMENT_DATA folder should have B1-B12 folders which should be empty
+ * Please refer README.txt for documentation
+ */
+public class Sentimental{
+  
+  public static void main(String[] args){
+    
+     /************************************************************************************************
+     * **********READS THE AFINN-111.CSV AND CREATS A HASH MAP WHERE THE WORD IS THE KEY***************
+     * *****************AND THE CORESSPONDING SENTIMENTAL VALUE IS THE VALUE************************
+     * *********************************************************************************************
+    */
+    
+    HashMap<String, Integer> afinn_hash_map = new HashMap<String, Integer>();
+    
+
+    try{
+    	
+    	FileReader fileRdr = new FileReader("AFINN-111.csv");
+       // System.out.println("OK. I opened a FileReader. Here's the file, line by line:\n");
+        
+        //Open a BufferedReader.
+        BufferedReader inFile = new BufferedReader(fileRdr);
+        
+        //Read the file line-by-line
+        String line;
+        do {
+          line = inFile.readLine();
+          if(line!=null) {
+        	  
+        	  String[] afinn = line.split(",");
+              String word = afinn[0];
+              int sentimentValue = Integer.parseInt(afinn[1]); 
+              afinn_hash_map.put(word, sentimentValue);
+          }
+        } while(line!=null);
+        inFile.close();
+    }//try
+   catch(IOException e){
+     
+    System.out.println("Failed to read the data file"+e.getMessage());
+    
+   }//catch
+   
+   /**********************************************************************************************
+     * *******************************************************************************************
+     * *******************************************************************************************
+     * *******************************************************************************************
+     */
+    
+    
+    
+    
+    /************************************************************************************************
+     * **********READS THE city.CSV AND CREATS ANOTHER FILE NAMED citySentiment.CSV***************
+     * **********WHERE THE tweet OF THE city.CSV IS REPLACED WITH ITS CORRESPONDINGVALUE******************
+     * *******************************SENTIMENTAL VALUE. ************************************************
+     * *********NOTE: DOWNLOAD THE RAW_DATA AND SENTIMENT_DATA FOLDER TO DESKTOP AND RUN THE CODE***************
+     * *********************************************************************************************
+    */
+    
+    String[] city = {"aberdeen","belfast","birmingham","cardiff","derry","edinburgh","glasgow","lisburn","liverpool","london","newport","swansea"};
+    int batches =12;
+    
+    for(int b =1; b < batches+1 ; b++) {
+    	for(int c =0; c < city.length; c++) {
+    
+try{
+    	
+    	FileReader fileRdr_2 = new FileReader("C:\\Desktop\\RAW_DATA\\B"+b+"\\"+city[c]+".csv");
+    	PrintWriter outFile = new PrintWriter(new FileWriter("C:\\Desktop\\SENTIMENT_DATA\\B"+b+"\\"+city[c]+"Sentiment.csv"));
+    	outFile.println("City,"+"Party,"+"Leader,"+"Sentiment Value");
+        //Open a BufferedReader.
+        BufferedReader inFile_2 = new BufferedReader(fileRdr_2);
+        
+        //Read the file line-by-line
+        String line_2;
+        line_2 = inFile_2.readLine();
+        while(line_2!=null) {
+          line_2 = inFile_2.readLine();
+          if(line_2!=null) {
+        	  String[] data = line_2.split(",");
+        	  if(data.length >3) {
+        		  String longTweet = "";
+        		  for (int i=3; i<data.length;i++) {
+        			  if(i==data.length) {
+        			  longTweet = (longTweet + data[i] + " " );
+        			  }//if
+        			  else {
+        				  longTweet = (longTweet + data[i]);
+        			  }//else
+        		  }//for
+        		  float sentimentValue = calculateSentiment(longTweet, afinn_hash_map);
+        		  outFile.println(data[0] + "," + data[1] + "," + data[2] +  "," + sentimentValue); //city party 
+        	  }//if
+          }//if
+        }//while
+        outFile.close();
+        inFile_2.close();
+    }//try
+   catch(IOException e){
+     
+    System.out.println("Failed to read the data file"+e.getMessage());
+    
+   }//catch
+    
+    
+    
+    /**********************************************************************************************
+     * *******************************************************************************************
+     * *******************************************************************************************
+     * *******************************************************************************************
+     */
+    	}//for changing city
+    }//for changing directory
+  }//main
+  
+  
+  /*
+   * Gives a sentimental value to each tweet by adding each words value from the AFINN-111.csv file and dividing the
+   * value with the length of the tweet (no. of words) so that we can give proper weight to the tweet.
+   */
+  public static float calculateSentiment(String tweet, HashMap<String, Integer> afinn_hash_map ) {
+	  float result = 0;
+	  tweet.toLowerCase();
+	  String[] tweetSplit = tweet.split("\\s+");
+	  
+	  
+	  for(int i = 0; i < tweetSplit.length; i++) {
+		  if(afinn_hash_map.containsKey(tweetSplit[i])) {
+			  result += afinn_hash_map.get(tweetSplit[i]);
+		  }//if
+		  else if((i < tweetSplit.length -1)&&(afinn_hash_map.containsKey(tweetSplit[i]+" "+tweetSplit[i+1]))) {
+			  result += afinn_hash_map.get(tweetSplit[i]+" "+tweetSplit[i+1]);  
+		  }//elseif
+		  else if((i < tweetSplit.length -2)&&(afinn_hash_map.containsKey(tweetSplit[i]+" "+tweetSplit[i+1]+" "+tweetSplit[i+2]))) {
+			  result += afinn_hash_map.get(tweetSplit[i]+" "+tweetSplit[i+1]+" "+tweetSplit[i+2]);  
+		  }//elseif
+		  
+	  }//for
+	  result = result/(tweetSplit.length);
+	  return result;
+  }//calculateSentiment
+  
+  
+}//Sentimental
